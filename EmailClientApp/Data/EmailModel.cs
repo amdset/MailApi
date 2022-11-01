b49d1df4a7c50025e6924c8e13e2c2c77fa8ea75
Misael Juarez Gordillo <gordillo95@outlook.com>
2022-10-31 23:26:22 -0600

    client Api

---
+++ EmailClientApp/Data/EmailModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmailClientApp.Data
+{
+    public class EmailModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("Firts Name")]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ EmailClientApp/Data/EmailService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace EmailClientApp.Data
+{
+    public class EmailService
+    {
+        HttpClient _httpClient = new HttpClient();
+        public EmailService()
+        {
+            _httpClient.BaseAddress = new Uri("https://emailapicoderslink.azurewebsites.net/");
+            _httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
+                );
+        }
+        public async Task<int> CreateEmailAsync(EmailModel email)
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/Email", email);
+            return (int)response.StatusCode;
+        }
+
+        public async Task<List<EmailModel>> GetEmails(string LastName = "", string orderBy = "Ascending")
+        {
+            List<EmailModel> emails = new List<EmailModel>();
+            HttpResponseMessage response = await _httpClient.GetAsync("api/Email");
+            var results = response.Content.ReadFromJsonAsync<List<EmailModel>>();
+            return results.Result;
+        }
+
+    }
+}
